7fabb7cb69848ac2dddade0594d6b25dd3e720da
Arun <agarwal.arun97@gmail.com>
2023-05-18 12:19:12 +0530

    Add the json validation variation

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Steps to run: 
 
-1) Create two folders woff and otf in root directory.
-2) Add your woff files in the woff folder.
-3) run command: npm install to install the dependancies.
+1) Create a folder lbr-configs in root directory.
+2) Add your json files in the lbr-configs folder.
+3) run command: npm install to install the dependencies.
+4) Uncomment the relevant piece of code.
 4) run command: npm run start.
--- index.js
+++ index.js
@@ -1,16 +1,70 @@
 const fs = require('fs');
-const woff2otf = require('woff2otf');
 
-  fs.readdir('woff/', function(err, filenames) {
-    if (err) {
-      onError(err);
-      return;
-    }
-      filenames.forEach(function (filename) {
-          const name = filename.split('.')[0];
+fs.readdir('lbr-configs/', function (err, filenames) {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    filenames.forEach(function (filename) {
+      if (!filename.startsWith('testing')) {
+        fs.readFile(`lbr-configs/${filename}`, 'utf8', (error, data) => {
+          if (error) {
+            console.log(error);
+            return;
+          }
+          else {
+            const obj = JSON.parse(data);
           
-        const woffFileBuffer = fs.readFileSync(`woff/${name}.woff`);
-        const otfFileBuffer = woff2otf(woffFileBuffer);
-        fs.writeFileSync(`otf/${name}.otf`, otfFileBuffer);
+            // Find the duplicate entires
+            // const uniq = obj.facetSet
+            //   .map((item) => {
+            //     return {
+            //       count: 1,
+            //       name: item.field
+            //     };
+            //   })
+            //   .reduce((result, b) => {
+            //     result[b.name] = (result[b.name] || 0) + b.count;
+
+            //     return result;
+            //   }, {});
+            // const duplicates = Object.keys(uniq).filter((a) => uniq[a] > 1);
+
+            // console.log('\n', duplicates, filename, 'redundant');
+
+            // For un-necessary active default filters
+            // const unNecessaryActiveFilters = obj.facetSet
+            //   .filter((item) => {
+            //     if (item.active === true) {
+            //       if (!(item.field === 'material.supplierLifecycleState' || item.field === 'material.supplierCode' || item.field === 'material.developer')) {
+            //         return true
+            //       }
+            //     }
+            //   })
+            // console.log(unNecessaryActiveFilters, filename, 'un-necessary active filter');
+
+            // For active default filters
+            // const missingNecessaryActiveFilters = obj.facetSet
+            //   .filter((item) => {
+            //     if (item.active === false) {
+            //       if ((item.field === 'material.supplierLifecycleState' || item.field === 'material.supplierCode' || item.field === 'material.developer')) {
+            //         return true
+            //       }
+            //     }
+            //   })
+            // console.log(missingNecessaryActiveFilters, filename, 'missing active filter');
+
+            // Check if the required column is not present 
+            // const unNecessaryColumn = obj.columnSet
+            //   .filter((item) => {
+            //     if ((item.field === 'material.supplier.usePurposes.productDivision')) {
+            //       return true
+            //     }
+            //   }).map(item => item.field);
+            // console.log(unNecessaryColumn, filename, 'un-necessary column'); 
+          }
+        });
+      }
     });
-  });
+  }
+});
